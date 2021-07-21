22214ef91d55c5241a86358b6c984c62a3cb28bf
joerg <joerg@yovisto.com>
2021-07-21 15:53:10 +0200

    Update webservice.py

--- src/webservice.py
+++ src/webservice.py
@@ -1,4 +1,4 @@
-import cherrypy, json, sys, cherrypy_cors
+import cherrypy, json, sys
 
 from predict import Prediction
 
@@ -30,7 +30,6 @@ if __name__ == '__main__':
 
    a = Prediction()
 
-   config = {'server.socket_host': '0.0.0.0', 'cors.expose.on': True}
-   cherrypy_cors.install()
+   config = {'server.socket_host': '0.0.0.0'}
    cherrypy.config.update(config)
    cherrypy.quickstart(WebService())	
