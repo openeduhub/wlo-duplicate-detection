823c4fd4ad572fd3f1c7ef653801a906510c6411
joerg <joerg@yovisto.com>
2021-09-23 10:45:40 +0200

    new dataset

--- runTraining.sh
+++ runTraining.sh
@@ -1,4 +1,4 @@
 cd data
-unzip -n wirlernenonline2-dedup.txt.zip
+unzip -n wirlernenonline3-dedup.txt.zip
 cd ..
 docker run -v `pwd`/data:/data -v `pwd`/src:/src wlo-dedup-py /usr/bin/python3 /src/createHashes.py 
--- src/createHashes.py
+++ src/createHashes.py
@@ -11,7 +11,7 @@ A MinHash-based near duplicate detection for the WLO dataset.
 """
 
 numHashes = 100;
-dataFile = "./data/wirlernenonline2-dedup.txt"
+dataFile = "./data/wirlernenonline3-dedup.txt"
 
 def shingleWords(words):
     # 'shinglesInDoc' will hold all of the unique shingle IDs present in the 
